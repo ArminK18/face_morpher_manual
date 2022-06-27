fe432bb4a13d03bda1da18e711049548a188f9fb
Armin18 <84017036+ArminK18@users.noreply.github.com>
2022-06-27 20:04:47 +0200

    Update face_alignment.py

--- code/utils/face_alignment.py
+++ code/utils/face_alignment.py
@@ -4,21 +4,17 @@ import os
 import PIL.Image
 
 def image_align(src_file, dst_file, face_landmarks, output_size=1024, transform_size=4096, enable_padding=True, x_scale=1, y_scale=1, em_scale=0.1, alpha=False):
-        # Align function from FFHQ dataset pre-processing step
-        # https://github.com/NVlabs/ffhq-dataset/blob/master/download_ffhq.py
-
         lm = np.array(face_landmarks)
-        lm_chin          = lm[0  : 17]  # left-right
-        lm_eyebrow_left  = lm[17 : 22]  # left-right
-        lm_eyebrow_right = lm[22 : 27]  # left-right
-        lm_nose          = lm[27 : 31]  # top-down
-        lm_nostrils      = lm[31 : 36]  # top-down
-        lm_eye_left      = lm[36 : 42]  # left-clockwise
-        lm_eye_right     = lm[42 : 48]  # left-clockwise
-        lm_mouth_outer   = lm[48 : 60]  # left-clockwise
-        lm_mouth_inner   = lm[60 : 68]  # left-clockwise
+        lm_chin = lm[0  : 17]
+        lm_eyebrow_left = lm[17 : 22]
+        lm_eyebrow_right = lm[22 : 27]
+        lm_nose = lm[27 : 31]
+        lm_nostrils = lm[31 : 36]
+        lm_eye_left = lm[36 : 42]
+        lm_eye_right = lm[42 : 48]
+        lm_mouth_outer = lm[48 : 60]
+        lm_mouth_inner = lm[60 : 68]
 
-        # Calculate auxiliary vectors.
         eye_left     = np.mean(lm_eye_left, axis=0)
         eye_right    = np.mean(lm_eye_right, axis=0)
         eye_avg      = (eye_left + eye_right) * 0.5
@@ -28,7 +24,6 @@ def image_align(src_file, dst_file, face_landmarks, output_size=1024, transform_
         mouth_avg    = (mouth_left + mouth_right) * 0.5
         eye_to_mouth = mouth_avg - eye_avg
 
-        # Choose oriented crop rectangle.
         x = eye_to_eye - np.flipud(eye_to_mouth) * [-1, 1]
         x /= np.hypot(*x)
         x *= max(np.hypot(*eye_to_eye) * 2.0, np.hypot(*eye_to_mouth) * 1.8)
@@ -38,13 +33,11 @@ def image_align(src_file, dst_file, face_landmarks, output_size=1024, transform_
         quad = np.stack([c - x - y, c - x + y, c + x + y, c + x - y])
         qsize = np.hypot(*x) * 2
 
-        # Load in-the-wild image.
         if not os.path.isfile(src_file):
             print('\nCannot find source image. Please run "--wilds" before "--align".')
             return
         img = PIL.Image.open(src_file).convert('RGBA').convert('RGB')
 
-        # Shrink.
         shrink = int(np.floor(qsize / output_size * 0.5))
         if shrink > 1:
             rsize = (int(np.rint(float(img.size[0]) / shrink)), int(np.rint(float(img.size[1]) / shrink)))
@@ -52,7 +45,6 @@ def image_align(src_file, dst_file, face_landmarks, output_size=1024, transform_
             quad /= shrink
             qsize /= shrink
 
-        # Crop.
         border = max(int(np.rint(qsize * 0.1)), 3)
         crop = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
         crop = (max(crop[0] - border, 0), max(crop[1] - border, 0), min(crop[2] + border, img.size[0]), min(crop[3] + border, img.size[1]))
@@ -60,7 +52,6 @@ def image_align(src_file, dst_file, face_landmarks, output_size=1024, transform_
             img = img.crop(crop)
             quad -= crop[0:2]
 
-        # Pad.
         pad = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
         pad = (max(-pad[0] + border, 0), max(-pad[1] + border, 0), max(pad[2] - img.size[0] + border, 0), max(pad[3] - img.size[1] + border, 0))
         if enable_padding and max(pad) > border - 4:
@@ -82,10 +73,8 @@ def image_align(src_file, dst_file, face_landmarks, output_size=1024, transform_
                 img = PIL.Image.fromarray(img, 'RGB')
             quad += pad[:2]
 
-        # Transform.
         img = img.transform((transform_size, transform_size), PIL.Image.QUAD, (quad + 0.5).flatten(), PIL.Image.BILINEAR)
         if output_size < transform_size:
             img = img.resize((output_size, output_size), PIL.Image.ANTIALIAS)
 
-        # Save aligned image.
         img.save(dst_file, 'PNG')
